8a9b09aa830c392100f58e609d96e42a67b08db7
Mike Daum <mike.daum@coobeeyon.net>
2023-12-11 20:45:06 -0500

    Day 4 Part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+use itertools::Itertools;
+
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -27,8 +29,29 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let card_scores = input
+        .lines()
+        .map(|l| l.split('|'))
+        .map(|v| {
+            v.map(|s| {
+                s.split(' ')
+                    .filter_map(|ns| ns.parse::<u32>().ok())
+                    .collect::<HashSet<u32>>()
+            })
+            .reduce(|a, s| a.intersection(&s).cloned().collect::<HashSet<_>>())
+            .unwrap_or(HashSet::new())
+        })
+        .map(|h| h.len())
+        .collect_vec();
+    let deck_size = card_scores.len();
+    let mut num_cards = vec![1; deck_size];
+    for i in 0..deck_size {
+        for j in i + 1..deck_size.min(i + card_scores[i] + 1) {
+            num_cards[j] += num_cards[i];
+        }
+    }
+    Some(num_cards.iter().sum())
 }
 
 #[cfg(test)]
@@ -44,6 +67,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
