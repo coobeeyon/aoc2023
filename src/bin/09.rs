befe497b8e8e12a1d0da9e6f601f641984cbc714
Mike Daum <mike.daum@coobeeyon.net>
2023-12-22 11:25:22 -0500

    Day 9 Part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -12,7 +12,6 @@ pub fn part_one(input: &str) -> Option<i64> {
                 .filter_map(|s| s.parse::<i64>().ok())
                 .collect_vec();
             successors(Some(nums), |last_nums| {
-                dbg!(&last_nums);
                 if last_nums.iter().all(|n| n == &0) {
                     None
                 } else {
@@ -32,8 +31,33 @@ pub fn part_one(input: &str) -> Option<i64> {
     Some(sum_diffs)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let sum_diffs = input
+        .lines()
+        .map(|line| {
+            let nums = line
+                .split(' ')
+                .filter_map(|s| s.parse::<i64>().ok())
+                .rev()
+                .collect_vec();
+            successors(Some(nums), |last_nums| {
+                if last_nums.iter().all(|n| n == &0) {
+                    None
+                } else {
+                    Some(
+                        last_nums
+                            .iter()
+                            .tuple_windows()
+                            .map(|(a, b)| b - a)
+                            .collect_vec(),
+                    )
+                }
+            })
+            .filter_map(|i| i.last().copied())
+            .sum::<i64>()
+        })
+        .sum::<i64>();
+    Some(sum_diffs)
 }
 
 #[cfg(test)]
@@ -49,6 +73,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
